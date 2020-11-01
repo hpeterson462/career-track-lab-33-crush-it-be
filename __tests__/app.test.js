ef62c3d7b3ee508b72c79e1089378124bab82a01
Heather Peterson <hpeterson462@gmail.com>
2020-11-01 12:27:34 -0800

    create route passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,15 @@ describe('crush-it routes', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
   });
+
+  it('creates a route', () => {
+    return request(app)
+      .post('/api/v1/routes')
+      .send({
+        location: 'Smith Rock',
+        name: 'Voyage of the Cowdog',
+        rating: '5.8+',
+        notes: 'Good warm-up.'
+      });
+  });
 });
